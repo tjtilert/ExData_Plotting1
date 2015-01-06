ff5da08711ab88c478ed80cc01ee4f6e1d3fec67
tjtilert <timothytilert@Timothys-MacBook-Pro.local>
2015-01-06 16:40:10 -0500

    First attempt - up to plot 2
    Read in the data, formatted the date fields, subset the data, and
    created the first two plots to specification.

---
+++ R code files/plot1.R
@@ -0,0 +1,47 @@
+
+#######################################################
+# Set the working directory
+#######################################################
+setwd("~/Dropbox/Coursera/Exploratory Data Analysis/Data")
+
+
+#######################################################
+# Read in the data file - 
+#  use classes and # of records for efficiency
+#######################################################
+classes=c(Date ="character", 
+          Time ="character",
+          Global_active_power = "numeric", 
+          Global_reactive_power = "numeric",
+          Voltage = "numeric",
+          Global_intensity = "numeric",
+          Sub_metering_1 = "numeric",
+          Sub_metering_2 = "numeric",
+          Sub_metering_3 = "numeric")
+
+hpc1 <- read.table("household_power_consumption.txt", header = TRUE, 
+          sep=";", na.strings="?", colClasses = classes, nrows = 2075259)
+
+str(hpc1)
+summary(hpc1)
+
+
+#######################################################
+# Convert character dates and times to a single R 
+# datetime (POSIXct) value, then subset only two dates:
+# 2007-02-01 and 2007-02-02
+#######################################################
+hpc1$r_datetime = as.POSIXct(paste(hpc1$Date, hpc1$Time), format="%d/%m/%Y %H:%M:%S")
+hpc = subset(hpc1, Date == "1/2/2007" | Date == "2/2/2007")
+
+
+#######################################################
+# Create the first plot (plot1.png)
+#######################################################
+setwd("~/Documents/ExData_Plotting1/plot files")
+png(filename = "plot1.png", width = 480, height = 480)
+
+hist(hpc$Global_active_power, col = "red", xlab="Global Active Power (kilowatts)",
+     main = "Global Active Power")
+
+dev.off()
